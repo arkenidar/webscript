fc752d2c9c34d8db241beafcd7d5dbc76b2c7ab7
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2022-12-06 13:23:04 +0100

    count and count-down (iterate numeric sequences)

--- iterations.js
+++ iterations.js
@@ -1,2 +1,7 @@
 for(var name of ["north", "south", "west", "east"] )
   console.log(name)
+
+for(var i=0; i<4; i++) console.log(i)
+
+console.info("countdown")
+for(var i=3; i>=0; i--) console.log(i)
--- iterations.php
+++ iterations.php
@@ -2,3 +2,8 @@
 $names = ["north", "south", "west", "east"];
 foreach( $names as $name)
   print("$name\n");
+
+for($i=0; $i<4; $i++) print("$i\n");
+
+print("countdown\n");
+for($i=3; $i>=0; $i--) print("$i\n");
