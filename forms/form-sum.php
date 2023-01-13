a2322a8978f502340fc052f3bb2711f990be9938
Dario "Arkenidar" Cangialosi <dario.cangialosi@gmail.com>
2023-01-13 11:12:40 +0100

    more robust input handling (BTW thanks to PHP's developer-server's messages). discovered and corrected.

--- forms/form-sum.php
+++ forms/form-sum.php
@@ -1,19 +1,28 @@
 <?php
-if(is_numeric($_REQUEST["n1"]) && is_numeric($_REQUEST["n2"]) )
-$result = intval($_REQUEST["n1"]) + intval($_REQUEST["n2"]);
-else
-$result = "not numeric inputs";
+if(isset($_REQUEST["n1"]) && isset($_REQUEST["n2"]) && is_numeric($_REQUEST["n1"]) && is_numeric($_REQUEST["n2"]) ){
+	
+	// if inputs are all valid then ...
+	$n1 = intval($_REQUEST["n1"]);
+	$n2 = intval($_REQUEST["n2"]);
+	$result = $n1 + $n2 ;
+
+}else{
+	$n1 = "";
+	$n2 = "";
+	$result = "not numeric inputs";	
+}
+
 ?><!doctype html>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta charset="UTF-8">
 
 <title>forms: 2 numbers summed up</title>
-<div>forms: 2 numbers summed up</div>
+<h1>forms: 2 numbers summed up</h1>
 
 result: <?= $result ?> <br>
 
 <form>
-<input name="n1" placeholder="first number" type="number" value="<?= $_REQUEST["n1"] ?>" >
-<input name="n2" placeholder="second number" type="number" value="<?= $_REQUEST["n2"] ?>" >
+<input name="n1" placeholder="first number" type="number" value="<?= $n1 ?>" >
+<input name="n2" placeholder="second number" type="number" value="<?= $n2 ?>" >
 <input type="submit" >
 </form>
